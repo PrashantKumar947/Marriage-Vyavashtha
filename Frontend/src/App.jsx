64518436672f0541051165ef816b390f693921eb
Prahant Kumar <palprahant947@gmail.com>
2024-07-19 19:17:34 +0530

    adding new compnents

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,17 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
+import Home from "./components/Home"
 
 
 const App = () =>{
   return <>
   <Navbar></Navbar>
+  <div className="home">
   <Sidebar/>
+  <Home></Home>
+  </div>
+  
   </>
 
 }
---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Styles from "./Home.module.css"
+function Home() {
+  return (
+   <>
+   <div id="carouselExampleCaptions" className="carousel slide" >
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="   https://images.unsplash.com/photo-1621801306185-8c0ccf9c8eb8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGluZGlhbiUyMG1hcnJpYWdlfGVufDB8fDB8fHww" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.unsplash.com/photo-1621801306185-8c0ccf9c8eb8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGluZGlhbiUyMG1hcnJpYWdlfGVufDB8fDB8fHww" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.unsplash.com/photo-1621801306185-8c0ccf9c8eb8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGluZGlhbiUyMG1hcnJpYWdlfGVufDB8fDB8fHww" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+   </>
+  )
+}
+
+export default Home
